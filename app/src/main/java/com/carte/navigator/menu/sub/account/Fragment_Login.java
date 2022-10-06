7081df5c42bcba7c5eb031f710e87c0e0145a6dd
Josh <njmkhari@gmail.com>
2022-10-06 11:09:09 +0200

    created navigation_account resourse file, fragments account, register and login

---
+++ app/src/main/java/com/carte/navigator/menu/sub/account/Fragment_Login.java
@@ -0,0 +1,25 @@
+package com.carte.navigator.menu.sub.account;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.carte.navigator.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class Fragment_Login extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
