2d3c0d795886e609a4517649746c6e59e34edc30
Darsh142001 <darshikapillay6@gmail.com>
2022-10-07 11:32:02 +0200

    Got the Registeration page working. Still have to connect it to firebase.
    I have created a project under firebase and connected this project to the Carte project in firebase.

--- app/src/main/java/com/carte/navigator/menu/sub/account/Fragment_Register.java
+++ app/src/main/java/com/carte/navigator/menu/sub/account/Fragment_Register.java
@@ -10,14 +10,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.carte.navigator.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
-public class Fragment_Register extends Fragment {
+public class Fragment_Register extends Fragment  {
 
 
     private EditText email;
@@ -26,11 +28,15 @@ public class Fragment_Register extends Fragment {
     private ImageButton signUp;
     private ImageButton loginWithEmail;
 
+    private FirebaseAuth mAuth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View register = inflater.inflate(R.layout.fragment_register, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
 
         email = register.findViewById(R.id.RegisterEmail_EditText);
         password = register.findViewById(R.id.RegisterPassword_EditText);
@@ -39,8 +45,58 @@ public class Fragment_Register extends Fragment {
         loginWithEmail = register.findViewById(R.id.RegisterLoginEmail_Button);
         signUp = register.findViewById(R.id.SignEmail_Button);
 
+        signUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                RegisterUser();
+            }
+        });
+
 
         loginWithEmail.setOnClickListener(v -> Navigation.findNavController(register).navigate(R.id.action_fragment_Register_to_fragment_Login));
         return register;
     }
+
+
+
+
+    public void RegisterUser()
+    {
+        String userEmail = email.getText().toString().trim();
+        String userPassword = password.getText().toString().trim();
+        String conPassword = confirmPassword.getText().toString().trim();
+
+        if(userEmail.isEmpty()){
+            email.setError("Email is required!");
+            email.requestFocus();
+            return;
+        }
+        if(userPassword.isEmpty()){
+            password.setError("Password is required!");
+            password.requestFocus();
+            return;
+        }
+        if(conPassword.isEmpty()){
+            confirmPassword.setError("Please confirm your password");
+            confirmPassword.requestFocus();
+            return;
+        }
+        if(userPassword.length() < 6 && conPassword.length() < 6){
+            password.setError("Min password length should be 6 characters");
+            password.requestFocus();
+            return;
+        }
+        if(!userPassword.equals(conPassword)){
+            confirmPassword.setError("Password does not match");
+            confirmPassword.requestFocus();
+            return;
+        }else if(userPassword.equals(userPassword)){
+            Toast.makeText(getActivity(), "You have registered successfully", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+
+
 }
