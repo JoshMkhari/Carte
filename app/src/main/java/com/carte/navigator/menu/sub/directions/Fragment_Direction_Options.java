3f0120994916fe5e52f4979baf5af2a98ba2cc4b
Josh <njmkhari@gmail.com>
2022-10-16 14:43:44 +0200

    Added OKHTTP client and recieving json from directions api

--- app/src/main/java/com/carte/navigator/menu/sub/directions/Fragment_Direction_Options.java
+++ app/src/main/java/com/carte/navigator/menu/sub/directions/Fragment_Direction_Options.java
@@ -1,66 +1,82 @@
 package com.carte.navigator.menu.sub.directions;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.carte.navigator.MainActivity;
+import com.carte.navigator.MapsFragment;
 import com.carte.navigator.R;
+import com.google.android.gms.maps.model.Marker;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_Direction_Options#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class Fragment_Direction_Options extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment_Direction_Options() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_Direction_Options.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment_Direction_Options newInstance(String param1, String param2) {
-        Fragment_Direction_Options fragment = new Fragment_Direction_Options();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_direction_options, container, false);
+        View direction_options =inflater.inflate(R.layout.fragment_direction_options, container, false);
+
+        //button_direction_start
+        //imageButton_close_direction_options_menu
+        //autoCompleteTextView_destination
+        //autoCompleteTextView_origin
+        //textview_temp
+
+        TextView temp = direction_options.findViewById(R.id.textview_temp);
+
+        //https://blog.logrocket.com/a-complete-guide-to-okhttp/
+        OkHttpClient client = new OkHttpClient();
+
+        Marker marker = MapsFragment._hashMapMarker.get(0);
+        assert marker != null;
+        Request request = new Request.Builder()
+                .url("https://trueway-directions2.p.rapidapi.com/FindDrivingRoute?stops="
+                        +MapsFragment._currentLocation.getLatitude() + "," + MapsFragment._currentLocation.getLongitude()+";"
+                        +marker.getPosition().latitude + "," + marker.getPosition().longitude)
+                .get()
+                .addHeader("X-RapidAPI-Key", "53834ed534msh54ac889e9f4d46dp160901jsn7121157456bc")
+                .addHeader("X-RapidAPI-Host", "trueway-directions2.p.rapidapi.com")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d("thisResponse", "onResponse: " + response.body().string());
+            }
+        });
+
+        return direction_options;
     }
+
+
 }
